13ac2da97dd96b6b39cc1fe4bde2cdcf293ebb70
RASHWANTH2005 <130953974+RASHWANTH2005@users.noreply.github.com>
2023-05-08 20:59:12 +0530

    Create day21.c

---
+++ day 21/day21.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        if (n % 4 == 0)
+            printf("Good\n");
+        else
+            printf("Not Good\n");
+    }
+    return 0;
+}
