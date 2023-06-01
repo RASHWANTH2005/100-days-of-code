83dd60f31465b8be7d90576f110100cdc08d5735
RASHWANTH2005 <130953974+RASHWANTH2005@users.noreply.github.com>
2023-06-01 23:06:09 +0530

    Create day39.c

---
+++ day 39/day39.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int T;
+    scanf("%d", &T);
+
+    while (T--)
+    {
+        int X, Y;
+        scanf("%d %d", &X, &Y);
+
+        int total = (X * 4) + Y;
+
+        printf("%d\n", total);
+    }
+
+    return 0;
+}
