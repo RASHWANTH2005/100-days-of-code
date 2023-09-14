1ffbd21a41399e3537c7725132d9987b50082a9c
RASHWANTH2005 <130953974+RASHWANTH2005@users.noreply.github.com>
2023-09-14 21:28:58 +0530

    Create day76.c

---
+++ day 76/day76.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdio.h>
+int main()
+{
+    int t,n,a,b,c;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d",&n);
+            a = n-2;
+            b=1;
+            c=1;
+        printf("%d %d %d\n",a,b,c);
+    }
+}
