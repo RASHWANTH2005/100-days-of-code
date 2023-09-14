370cbc964e1ce49e94cac1df535ebd7abba83e80
RASHWANTH2005 <130953974+RASHWANTH2005@users.noreply.github.com>
2023-09-14 21:36:39 +0530

    Create day77.c

---
+++ day 77/day77.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long int x,y,t;
+	scanf("%ld %ld %ld", &x, &y, &t);
+	long int a=t-x;
+	long int b=a/y;
+	
+	if(b%2==0)
+	{
+	    printf("Lucky Chef\n");
+	}
+	else
+	{
+	    printf("Unlucky Chef\n");
+	}
+	return 0;
+}
+
