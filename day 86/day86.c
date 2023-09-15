7637b77be4c2f7b80f877367f4efa436d41aed55
RASHWANTH2005 <130953974+RASHWANTH2005@users.noreply.github.com>
2023-09-15 20:51:00 +0530

    Create day86.c

---
+++ day 86/day86.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int t;
+  scanf("%i", &t);
+  while (t--)
+  {
+    int n, m, n2, m2;
+    scanf("%d %d", &n, &m);
+    n2 = n % 2;
+    m2 = m % 2;
+    if ((n2 == 0) && (m2 == 0))
+    {
+      printf("0\n");
+    }
+    else if (n2 == 0)
+    {
+      printf("%d\n", n);
+    }
+    else if (m2 == 0)
+    {
+      printf("%d\n", m);
+    }
+    else
+    {
+      printf("%d\n", n + m - 1);
+    }
+  }
+  return 0;
+}
+
+
